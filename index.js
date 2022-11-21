325fbdef1308ebc15631d704c8c53f03e85e9ad8
CesarNahra <105174608+CesarNahra@users.noreply.github.com>
2022-11-21 14:35:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let msg = "Hello World";
+let express = require("express");
+let app = express();
 
-console.log(msg);
+app.get("/", function(req, res) {
+    res.send("Primeira rota com Express");
+});
+
+app.get("/teste", function(req, res) {
+    res.send("Testando rota");
+});
+
+app.listen(3000, function() {
+    console.log("A aplicação está funcionando na rota 3000");
+});
