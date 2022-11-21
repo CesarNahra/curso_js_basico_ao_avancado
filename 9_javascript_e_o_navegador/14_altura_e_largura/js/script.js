bb1ec53763242f5d8ebbee590e9d17bced9d32cf
CesarNahra <105174608+CesarNahra@users.noreply.github.com>
2022-11-21 14:32:05 -0300

    Add files via upload

---
+++ 9_javascript_e_o_navegador/14_altura_e_largura/js/script.js
@@ -0,0 +1,7 @@
+let elemento = document.querySelector("#titulo-principal");
+
+console.log(elemento.offsetWidth);
+console.log(elemento.offsetHeight);
+
+console.log(elemento.clientWidth);
+console.log(elemento.clientHeight);
